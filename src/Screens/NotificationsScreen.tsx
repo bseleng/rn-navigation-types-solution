da927ec7ae66376c0115a04516f0f26fa7cd985f
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-05-28 11:17:28 +0300

    feat(!): add basic stack navigator and screens
    BREAKING CHANGE: TS2786: 'Stack.Navigator' cannot be used as a JSX component.

---
+++ src/Screens/NotificationsScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+
+const NotificationsScreen = () => {
+  return (
+    <View>
+      <Text>Notifications</Text>
+    </View>
+  )
+}
+
+export default NotificationsScreen
---
+++ src/StackNavigator/StackNavigatorBasic.tsx
@@ -0,0 +1,20 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../Screens/HomeScreen';
+import NotificationsScreen from '../Screens/NotificationsScreen';
+import ProfileScreen from "../Screens/ProfileScreen";
+import SettingsScreen from '../Screens/SettingsScreen';
+
+const Stack = createStackNavigator();
+
+const StackNavigatorBasic =() => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default StackNavigatorBasic
